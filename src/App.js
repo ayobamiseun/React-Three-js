cb40cee8f14754e60c53168dc39129dcd68fb0e8
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-03-27 12:18:38 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      <h1 className='font-medium px-3 py-2 text-red-700  rounded-lg hover:bg-slate-100 hover:text-slate-900'>Hi welcome to my App Testing </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
