f754bff417e2aa3ddb4946652ce2f26ec1bf6548
ayobamiseun <adegokeayobamiseun@gmail.com>
2023-04-03 09:43:52 +0100

    drei install

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-
-import './App.css';
-
+ import { BrowserRouter } from "react-router-dom";
+ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-      <h1 className='font-medium px-3 py-2 text-red-700  rounded-lg hover:bg-slate-100 hover:text-slate-900'>Hi welcome to my App Testing </h1>
-      </header>
+ 
+   <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+    <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+      <Navbar/>
+      <Hero />
     </div>
+    <About/>
+    <Experience />
+     <Tech />
+     <Works />
+     <Feedbacks />
+     <div className="relative z-0">
+       <Contact />
+       <StarsCanvas />
+     </div>
+    </div>
+   </BrowserRouter>
+  
   );
 }
 
